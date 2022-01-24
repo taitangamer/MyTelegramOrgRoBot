24340db84ea861932dc368b74091d8c1dc1093e2
Pʀᴇᴅᴀᴛᴏʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-01-24 09:51:13 +0530

    Update step_two.py

--- helper_funcs/step_two.py
+++ helper_funcs/step_two.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (c) Shrimadhav U K
+# Copyright (c) Shrimadhav U K | PredatorHackerzZ 😂
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
